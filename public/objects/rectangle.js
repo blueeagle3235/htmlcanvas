391b8e23c3785f6f1be9a2f014c91709acef4e18
givemepower8 <givemepower8@gmail.com>
2021-11-12 22:39:18 -0800

    minor

--- public/objects/rectangle.js
+++ public/objects/rectangle.js
@@ -1,29 +1,34 @@
-function Rectangle(width,height){
-    if (width===undefined) {scale=30;}
-    if (height===undefined) {scale=15;}
-    this.x=0;
-    this.y=0;
-    this.rotation=0;
-    this.width=width;
-    this.height=height;
-    this.scaleX=1;
-    this.scaleY=1;
-    this.lineWidth=0;
+function Rectangle(width, height, x, y, color) {
+  if (width === undefined) {
+    scale = 30;
+  }
+  if (height === undefined) {
+    scale = 15;
+  }
+  this.x = x;
+  this.y = y;
+  this.rotation = 0;
+  this.width = width;
+  this.height = height;
+  this.scaleX = 1;
+  this.scaleY = 1;
+  this.lineWidth = 0;
+  this.color = color;
 }
-Rectangle.prototype.draw=function(context){
-    context.save();
-    
-    context.translate(this.x, this.y);
-    context.rotate(this.rotation);
-    context.scale(this.scaleX, this.scaleY);
-    context.lineWidth = 2;
-    context.fillStyle='black';
+Rectangle.prototype.draw = function (context) {
+  context.save();
 
-    context.beginPath();
-    context.rect(0, 0, this.width, this.height);
-    context.closePath();
-    context.fill();
-    context.stroke();
+  context.translate(this.x, this.y);
+  context.rotate(this.rotation);
+  context.scale(this.scaleX, this.scaleY);
+  //context.lineWidth = 2;
+  context.fillStyle = this.color;
 
-    context.restore();
-}
+  context.beginPath();
+  context.rect(0, 0, this.width, this.height);
+  context.closePath();
+  context.fill();
+  context.stroke();
+
+  context.restore();
+};
